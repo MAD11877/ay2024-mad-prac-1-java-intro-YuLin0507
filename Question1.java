3d93b3dfcca6e119539447b4f18f6e808dc8df2e
Tan Yu Lin <148183690+YuLin0507@users.noreply.github.com>
2024-04-15 14:37:20 +0800

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -2,10 +2,9 @@ public class Question1
 {
   public static void main(String[] args)
   {
-    /**
-     * Print "Hello World!"
-     * 
-     * Hint: System.out.println(...)
-     */
+    public static void main(String[] args)
+  {    
+    System.out.println("Hello World!")
+  }
   }
 }
