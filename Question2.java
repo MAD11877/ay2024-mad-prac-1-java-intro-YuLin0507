a2e0314fc0146fa550689d8d519851eea2ea3291
Tan Yu Lin <148183690+YuLin0507@users.noreply.github.com>
2024-04-15 14:58:40 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -1,25 +1,24 @@
 import java.util.Scanner;
 
-public class Question2
-{
-  public static void main(String[] args)
-  {  
-    Scanner in = new Scanner(System.in);
-    
-    System.out.print("Please enter your height (in meters): ");
-    double height = in.nextDouble();
-    System.out.println("Your height is: " + height + " meters.");
+public class Question2 {
+    public static void main(String[] args) {  
+        Scanner in = new Scanner(System.in);
+        
+        System.out.print("Please enter your height (in meters): ");
+        double height = in.nextDouble();
+        System.out.println("Your height is: " + height + " meters.");
 
-    System.out.print("Please enter your weight (in kg): ");
-    double weight = in.nextDouble();
-    System.out.println("Your weight is: " + weight + " kilograms.");
+        System.out.print("Please enter your weight (in kg): ");
+        double weight = in.nextDouble();
+        System.out.println("Your weight is: " + weight + " kilograms.");
 
-    double bmi = calculateBMI(weight, height);
+        double bmi = calculateBMI(weight, height);
 
-    System.out.println("Your BMI is: " + bmi);
+        System.out.println("Your BMI is: " + bmi);
+    }
 
     public static double calculateBMI(double weight, double height) {
         return weight / (height * height);
     }
-  }
 }
+
