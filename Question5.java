28fc983d2ef65b909b89432af138d006e789de53
Tan Yu Lin <148183690+YuLin0507@users.noreply.github.com>
2024-04-15 15:32:40 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -27,6 +27,31 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
+
+    int count = in.nextInt();
+        
+        int[] numbers = new int[count];
+        int[] frequencies = new int[101]; // Assuming numbers range from 0 to 100
+        
+        // Loop to get input and update frequencies array
+        for (int i = 0; i < count; i++) {
+            int num = in.nextInt();
+            numbers[i] = num;
+            frequencies[num]++;
+        }
+        
+        // Find the mode (number with highest frequency)
+        int mode = -1;
+        int maxFrequency = 0;
+        for (int i = 0; i < frequencies.length; i++) {
+            if (frequencies[i] > maxFrequency) {
+                maxFrequency = frequencies[i];
+                mode = i;
+            }
+        }
+        
+        // Print the mode
+        System.out.println(mode);
     
   }
 }
